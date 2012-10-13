2a91f2b00bfcd42621aad4301f78fa9cb5881010
Max Thrun <bear24rw@gmail.com>
2012-10-12 22:55:24 -0400

    [rom_flasher] exit on error / initialize last sent byte so we don't error on the first byte

--- fpga/rom_flasher/tools/send_rom.py
+++ fpga/rom_flasher/tools/send_rom.py
@@ -12,7 +12,8 @@ print "Rom size: %d" % rom_size
 rom = open(sys.argv[1], 'rb')
 
 bytes_sent = 0
-last_byte_sent = 0
+percent_sent = 0.0
+last_byte_sent = chr(0)
 
 print "Waiting for FPGA to request bytes..."
 
@@ -25,14 +26,15 @@ for byte in rom.read():
     s.write(byte)
 
     bytes_sent += 1
+    percent_sent = float(bytes_sent)/float(rom_size)*100.0
 
     # we should receive back the last byte we sent as an ACK
     if (recv != last_byte_sent):
-        error = "RECIEVED BYTE DOES NOT MATCH!"
-    else:
-        error = ""
+        print "RECIEVED BYTE DOES NOT MATCH!"
+        print "Recv: %2X | Last Sent: %2X" % (ord(recv), ord(last_byte_sent))
+        sys.exit()
 
-    print "Sent %d of %d bytes (sent: %2X | recv: %2X) %s    \r" % \
-            (bytes_sent, rom_size, ord(byte), ord(recv), error)
+    print "[%.2f] %d / %d (sent: %2X | recv: %2X)" % \
+            (percent_sent, bytes_sent, rom_size, ord(byte), ord(recv) )
 
     last_byte_sent = byte
