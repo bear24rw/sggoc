7d58850554a8f00168cb4747b42b4bbc173b313f
Samir Silbak <silbak04@gmail.com>
2012-11-06 01:02:54 -0500

    [verilog] fixed vga timing module

--- fpga/vga_controller/rtl/vga_timing.v
+++ fpga/vga_controller/rtl/vga_timing.v
@@ -4,58 +4,44 @@ module vga_timing (
     input clk_50,
     input rst,
 
-    //output reg [3:0] vga_r = 0,
-    //output reg [3:0] vga_g = 0,
-    //output reg [3:0] vga_b = 0,
-
-    output reg [9:0] h_sync_cnt = 0,
-    output reg [9:0] v_sync_cnt = 0,
-
     output reg vga_hs = 0,
     output reg vga_vs = 0,
 
-    output reg vga_clk = 0
+    output reg vga_clk = 0,
+
+    output reg [9:0] h_sync_cnt = 0,
+    output reg [9:0] v_sync_cnt = 0
 );
 
-    initial begin
-        h_sync_cnt = 1;
-        v_sync_cnt = 1;
-    end
-    
     always @(posedge clk_50)
         vga_clk = ~vga_clk;
 
     always @(posedge vga_clk) begin
         if (rst) begin
-
             h_sync_cnt <= 0;
             v_sync_cnt <= 0;
-            vga_hs     <= 0;
-            vga_vs     <= 0;
-
+            vga_hs     <= 1;
+            vga_vs     <= 1;
         end else begin
 
             if (h_sync_cnt > `h_sync_pulse_cnt)
-                vga_hs <= 1;
-            else
                 vga_hs <= 0;
+            else
+                vga_hs <= 1;
 
             if (h_sync_cnt == `h_line_cnt) begin
                 h_sync_cnt <= 0;
-                vga_hs     <= 0;
+                v_sync_cnt <= v_sync_cnt + 1;
             end else
                 h_sync_cnt <= h_sync_cnt + 1;
                 
             if (v_sync_cnt > `v_sync_pulse_cnt) 
-                vga_vs <= 1;
-            else
                 vga_vs <= 0;
+            else
+                vga_vs <= 1;
 
-            if (v_sync_cnt == `v_fram_cnt) begin
+            if (v_sync_cnt == `v_fram_cnt) 
                 v_sync_cnt <= 0;
-                vga_vs     <= 0;
-            end else
-                v_sync_cnt <= v_sync_cnt + 1;
 
         end
 
