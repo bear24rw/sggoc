2de31f9df4df0ae7d5d974c6753bf3ccf5172ba9
Samir Silbak <silbak04@gmail.com>
2012-11-06 01:31:51 -0500

    [verilog] added debug pins

--- fpga/vga_controller/rtl/top.v
+++ fpga/vga_controller/rtl/top.v
@@ -3,9 +3,13 @@
 module top (
     input CLOCK_50,
     input [3:0] KEY,
+
     output [3:0] VGA_R,
     output [3:0] VGA_G,
     output [3:0] VGA_B,
+
+    output [1:0] GPIO_1,
+    
     output VGA_HS,
     output VGA_VS
 );
@@ -25,30 +29,30 @@ module top (
     wire vga_hs;
     wire vga_vs;
 
-    always @ (posedge vga_clk) begin
-        if (h_sync_cnt == 600 && v_sync_cnt == 400) begin
+    always @(posedge vga_clk) begin
+        if (h_sync_cnt >= `h_visible_cnt && h_sync_cnt < `h_back_porch_cnt &&
+            v_sync_cnt >= `v_visible_cnt && v_sync_cnt < `v_back_porch_cnt) begin
             vga_r <= 4'd15;
-            vga_g <= 4'd0;
-            vga_b <= 4'd0;
+            vga_g <= 4'd00;
+            vga_b <= 4'd00;
         end
-
     end
 
     assign VGA_R = vga_r;
     assign VGA_G = vga_g;
     assign VGA_B = vga_b;
 
-vga_timing vga_timing (
-    .clk_50(clk),
-    .rst(rst),
-    //.vga_r(VGA_R),
-    //.vga_g(VGA_G),
-    //.vga_b(VGA_B),
-    .vga_hs(VGA_HS),
-    .vga_vs(VGA_VS),
-    .h_sync_cnt(h_sync_cnt),
-    .v_sync_cnt(v_sync_cnt),
-    .vga_clk(vga_clk)
-);
+    assign GPIO_1[0] = VGA_HS;
+    assign GPIO_1[1] = VGA_VS;
+
+    vga_timing vga_timing (
+        .clk_50(clk),
+        .rst(rst),
+        .vga_hs(VGA_HS),
+        .vga_vs(VGA_VS),
+        .h_sync_cnt(h_sync_cnt),
+        .v_sync_cnt(v_sync_cnt),
+        .vga_clk(vga_clk)
+    );
 
 endmodule
