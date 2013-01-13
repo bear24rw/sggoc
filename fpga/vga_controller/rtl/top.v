dd494713493c35cdff69fedb889b3d4365dc40dc
Max Thrun <bear24rw@gmail.com>
2013-01-13 01:32:13 -0500

    [vga] fixed scan_x scan_y naming / added pixel_xy outputs / color test pattern

--- fpga/vga_controller/rtl/top.v
+++ fpga/vga_controller/rtl/top.v
@@ -17,24 +17,55 @@ module top (
     wire rst = ~(KEY[0]);
     wire clk = CLOCK_50;
 
-    wire [9:0] scan_y;
-    wire [9:0] scan_x;
+    wire [9:0] y;
+    wire [9:0] x;
 
     reg [3:0] vga_r = 0;
     reg [3:0] vga_g = 0;
     reg [3:0] vga_b = 0;
+    reg [5:0] fade = 0;
 
     wire vga_clk;
-
-    wire vga_hs;
-    wire vga_vs;
+    wire in_display_area;
 
     always @(posedge vga_clk) begin
-        if (scan_y < `h_visible_cnt &&
-            scan_x < `v_visible_cnt) begin
-            vga_r <= 4'd15;
-            vga_g <= 4'd00;
-            vga_b <= 4'd00;
+        if (in_display_area) begin
+            if (x > 50 && x < 100) begin
+                vga_r <= 4'hF;
+                vga_g <= 4'hF;
+                vga_b <= 4'hF;
+            end else if (x > 100 && x < 150) begin
+                vga_r <= 4'hF;
+                vga_g <= 4'h0;
+                vga_b <= 4'h0;
+            end else if (x > 150 && x < 200) begin
+                vga_r <= 4'h0;
+                vga_g <= 4'hF;
+                vga_b <= 4'h0;
+            end else if (x > 200 && x < 250) begin
+                vga_r <= 4'h0;
+                vga_g <= 4'h0;
+                vga_b <= 4'hF;
+            end else if (x > 250 && x < 300) begin
+                fade <= (300-x);
+                vga_r <= fade[5:2];
+                vga_g <= 4'h0;
+                vga_b <= 4'h0;
+            end else if (x > 300 && x < 350) begin
+                fade <= (350-x);
+                vga_r <= 4'h0;
+                vga_g <= fade[5:2];
+                vga_b <= 4'h0;
+            end else if (x > 350 && x < 400) begin
+                fade <= (400-x);
+                vga_r <= 4'h0;
+                vga_g <= 4'h0;
+                vga_b <= fade[5:2];
+            end else begin
+                vga_r <= 4'h0;
+                vga_g <= 4'h0;
+                vga_b <= 4'h0;
+            end
         end else begin
             vga_r <= 4'd00;
             vga_g <= 4'd00;
@@ -54,8 +85,9 @@ module top (
         .rst(rst),
         .vga_hs(VGA_HS),
         .vga_vs(VGA_VS),
-        .scan_y(scan_y),
-        .scan_x(scan_x),
+        .pixel_y(y),
+        .pixel_x(x),
+        .in_display_area(in_display_area),
         .vga_clk(vga_clk)
     );
 
--- fpga/vga_controller/rtl/vga_timing.v
+++ fpga/vga_controller/rtl/vga_timing.v
@@ -8,43 +8,49 @@ module vga_timing (
     output reg vga_vs = 0,
 
     output reg vga_clk = 0,
-
-    output reg [9:0] scan_y = 0,
-    output reg [9:0] scan_x = 0
+    output [9:0] pixel_x,
+    output [9:0] pixel_y,
+    output in_display_area
 );
+
+    reg [9:0] scan_x = 0;
+    reg [9:0] scan_y = 0;
 
     always @(posedge clk_50)
         vga_clk = ~vga_clk;
 
     always @(posedge vga_clk) begin
         if (rst) begin
-            scan_y <= 0;
             scan_x <= 0;
-            vga_hs <= 1;
-            vga_vs <= 1;
+            scan_y <= 0;
+            vga_hs <= 0;
+            vga_vs <= 0;
         end else begin
 
-            if (scan_y > `h_sync_pulse_cnt)
+            if (scan_x == `h_line_cnt) begin
+                scan_x <= 0;
+                scan_y <= scan_y + 1;
+            end else
+                scan_x <= scan_x + 1;
+
+            if (scan_y == `v_fram_cnt)
+                scan_y <= 0;
+
+            if (scan_x > `h_sync_pulse_cnt)
                 vga_hs <= 1;
             else
                 vga_hs <= 0;
-
-            if (scan_y == `h_line_cnt) begin
-                scan_y <= 0;
-                scan_x <= scan_x + 1;
-            end else
-                scan_y <= scan_y + 1;
                 
-            if (scan_x > `v_sync_pulse_cnt) 
+            if (scan_y > `v_sync_pulse_cnt)
                 vga_vs <= 1;
             else
                 vga_vs <= 0;
 
-            if (scan_x == `v_fram_cnt) 
-                scan_x <= 0;
-
         end
-
     end
+
+    assign pixel_x = scan_x - (`h_sync_pulse_cnt + `h_fron_porch_cnt);
+    assign pixel_y = scan_y - (`v_sync_pulse_cnt + `v_fron_porch_cnt);
+    assign in_display_area = (scan_x < `h_visible_cnt) && (scan_y < `v_visible_cnt);
 
 endmodule
