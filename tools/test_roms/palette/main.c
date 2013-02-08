ca59d7958d579f6fca22ab46a046a24b57deef9e
Max Thrun <bear24rw@gmail.com>
2013-02-08 17:52:23 -0500

    [roms] set palette 0 to pure red

--- tools/test_roms/palette/main.c
+++ tools/test_roms/palette/main.c
@@ -5,17 +5,16 @@ __sfr __at (0xBF) vdp_control;
 
 int main()
 {
-    int x;
-
     while (1) {
         // set register 1 bit 6 to enable display
         vdp_control = 1 << 6;
         vdp_control = 0x80 | 0x01;
 
-        // set first palette entry to 3
+        // set first palette entry to red
         vdp_control = 0x00;
         vdp_control = 0xC0;
-        vdp_data = 0x03;
+        vdp_data = 0x0F;
+        vdp_data = 0x00;
     }
 
     return 0;
