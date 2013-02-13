5c8bde537ce2ae76152780b17ce59ae0eca35881
Max Thrun <bear24rw@gmail.com>
2013-02-12 22:52:54 -0500

    [test_roms] set 4 palette colors and count up on the debug port

--- tools/test_roms/palette/main.c
+++ tools/test_roms/palette/main.c
@@ -2,19 +2,40 @@
 
 __sfr __at (0xBE) vdp_data;
 __sfr __at (0xBF) vdp_control;
+__sfr __at (0x01) debug;
 
 int main()
 {
+    uint8_t x = 0;
+
     while (1) {
         // set register 1 bit 6 to enable display
         vdp_control = 1 << 6;
         vdp_control = 0x80 | 0x01;
 
-        // set first palette entry to red
+        // start at palette entry 0
         vdp_control = 0x00;
         vdp_control = 0xC0;
+
+        // red
         vdp_data = 0x0F;
         vdp_data = 0x00;
+
+        // green
+        vdp_data = 0xF0;
+        vdp_data = 0x00;
+
+        // blue
+        vdp_data = 0x00;
+        vdp_data = 0x0F;
+
+        // white
+        vdp_data = 0xFF;
+        vdp_data = 0x0F;
+
+        // update the debug leds
+        debug = x;
+        x++;
     }
 
     return 0;
