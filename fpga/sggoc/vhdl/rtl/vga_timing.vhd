167a513dd2cfc419976d7c8fad74f9cab95a6254
Samir Silbak <silbak04@gmail.com>
2013-05-26 14:59:11 -0400

    [vga_timing] changed scan_x/y back to std_logic_vectors

--- fpga/sggoc/vhdl/rtl/vga_timing.vhd
+++ fpga/sggoc/vhdl/rtl/vga_timing.vhd
@@ -55,10 +55,8 @@ end vga_timing;
 
 architecture rtl of vga_timing is
 
-    signal scan_x : natural range 0 to 2**10-1 := 0; -- easier for incrementation...
-    signal scan_y : natural range 0 to 2**10-1 := 0; -- easier for incrementation...
-    --signal scan_x : std_logic_vector (9 downto 0) := (others => '0');
-    --signal scan_y : std_logic_vector (9 downto 0) := (others => '0');
+    signal scan_x : std_logic_vector (9 downto 0) := (others => '0');
+    signal scan_y : std_logic_vector (9 downto 0) := (others => '0');
 
     signal clk_25 : std_logic := '0';
 
@@ -73,18 +71,19 @@ begin
 
     process(clk_25, rst) begin
         if (rst = '1') then
-            scan_x <= 0;
-            scan_y <= 0;
+            scan_x <= (others => '0');
+            scan_y <= (others => '0');
         elsif rising_edge(clk_25) then
             if (scan_x = h_line_cnt-1) then
-                scan_x <= 0;
-                scan_y <= scan_y + 1;
+                scan_x <= (others => '0');
+                scan_y <= slv(unsigned(scan_y) + 1);
             else
-                scan_x <= scan_x + 1;
+                scan_x <= slv(unsigned(scan_x) + 1);
+                scan_x <= slv(unsigned(scan_x) + 1);
             end if;
 
             if (scan_y = v_fram_cnt) then
-                scan_y <= 0;
+                scan_y <= (others => '0');
             end if;
         end if;
     end process;
@@ -114,7 +113,7 @@ begin
 
    vga_clk <= clk_25;
 
-   pixel_x <= slv(to_unsigned(scan_x, pixel_x'length) - (h_sync_pulse_cnt + h_fron_porch_cnt));
-   pixel_y <= slv(to_unsigned(scan_y, pixel_y'length) - (v_sync_pulse_cnt + v_fron_porch_cnt));
+   pixel_x <= slv(unsigned(scan_x) - (h_sync_pulse_cnt + h_fron_porch_cnt));
+   pixel_y <= slv(unsigned(scan_y) - (v_sync_pulse_cnt + v_fron_porch_cnt));
 
 end rtl;
